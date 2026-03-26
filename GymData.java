ac96d35a0661ccc880f7b2224c5c38f4124d575b
chijiokedavid117-cpu <chijiokedavid117@gmail.com>
2026-03-26 15:40:54 -0300

    Add files via upload

---
+++ GymData.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+/**
+ * Write a description of class GymData here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GymData
+{
+  public static void main(String[] args)
+  {
+
+        try 
+        {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/gymdb",
+                "root",
+                "Doc242007"
+            );
+
+            System.out.println("Connected!");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Member");
+
+            while (rs.next())
+            {
+                System.out.println(rs.getString("name"));
+            }
+
+        } catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
+
